f3d0b495174c44bbcf42eebe5bd3c5113c91917e
4n0ny_m0 <127933255+MorenoLopez@users.noreply.github.com>
2026-07-13 09:48:46 +0000

    ex1

---
+++ ex1/ft_garden_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_garden_name.py                                    :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/04/08 10:14:56 by horarivo            #+#    #+#            #
+#   Updated: 2026/04/08 10:15:47 by horarivo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+def ft_garden_name() -> None:
+    text = input("Enter garden name: ")
+    print(f"Garden: {text}\nStatus: Growing well!")
