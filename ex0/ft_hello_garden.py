a5453263ac54be9b3aa29e5162137ec9fd46fb66
4n0ny_m0 <127933255+MorenoLopez@users.noreply.github.com>
2026-07-13 09:47:42 +0000

    ex0

---
+++ ex0/ft_hello_garden.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_hello_garden.py                                   :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: horarivo <horarivo@student.42antananarivo.   +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/04/08 10:14:35 by horarivo            #+#    #+#            #
+#   Updated: 2026/04/09 21:22:30 by horarivo           ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+def ft_hello_garden() -> None:
+    print("Hello, Garden Community!")
